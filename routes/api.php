32383e71f6cb2c89a205ba259c1045df29c673c8
csynoers <3s0c9m7@gmail.com>
2019-03-05 15:14:48 +0700

    CRUD use vue JS

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// list articles
+Route::get('articles','ArticleController@index');
+
+// single article
+Route::get('article/{id}','ArticleController@show');
+
+// create new article
+Route::post('article','ArticleController@store');
+
+// update article
+Route::put('article','ArticleController@store');
+
+// delete article
+Route::delete('article/{id}','ArticleController@destroy');
